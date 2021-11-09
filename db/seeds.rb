215a6289bc3824ade6775e74834a7e6cdfec0078
Diana Baiadori <diana.baiadori@gmail.com>
2021-11-09 16:54:43 +0100

    rails wikinimous exercise done w/o style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(title: Faker::Science.modifier,
+                        content: Faker::String.random.to_s)
+  article.save
+end
+
+# articles = article.all
